7abffab20882134097b00fe4e5d1908a4f1d2601
Daniel <djp21795@yahoo.com>
2020-08-03 21:33:47 -0400

    First Commit

---
+++ VotingApp/VotingHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace VotingApp
+{
+    public class VotingHub : Hub
+    {
+
+        public static Dictionary<string, int> poll = new Dictionary<string, int>(){
+             {"Dan",0 },
+             {"Ray",0},
+             {"John",0},
+             {"Mom",0},
+             {"Dad",0},        
+        };
+
+
+        public void Send(string name)
+        {
+            poll[name]++;
+            string data = JsonConvert.SerializeObject(poll.Select(x => new { name = x.Key, count = x.Value }).ToList());
+
+
+            Clients.All.showLiveResult(data);
+        }
+    }
+
+}
